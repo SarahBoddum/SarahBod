bce65e42e151e6d18dd171e5beebdf7f469e4b11
SarahBoddum <126453046+SarahBoddum@users.noreply.github.com>
2026-09-09 20:04:52 +0200

    No fucks og commit fra pkt 13

--- server.js
+++ server.js
@@ -5,22 +5,59 @@ const PORT = 3000;
 const messages = [];
 
 
+const answers = [
+  {
+    keywords: ["navn", "hedder", "hvem er du"],
+    answer: "Jeg hedder Sarah. Hvad vil du ellers vide om mig?"
+  },
+  {
+    keywords: ["bor", "by", "fra"],
+    answer: "Jeg bor i Beder."
+  },
+  {
+    keywords: ["fritid", "hobby", "kan lide"],
+    answer: "Jeg har en virksomhed på hobbyniveau (...tror Ase) hvor jeg syr og designer tøj. I virkeligheden er det nok ok meget mere end hobby. Shhhh. Ellers broderer jeg mega meget for tiden, tegner og løber"
+  }
+];
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+
+function findAnswer(question) {
+  const normalizedQuestion = question.toLowerCase();
+
+  for (const answerGroup of answers) {
+    const hasMatch = answerGroup.keywords.some((keyword) => normalizedQuestion.includes(keyword));
+
+    if (hasMatch) {
+      return answerGroup.answer;
+    }
+  }
+
+  return "Det kender jeg ikke svaret på endnu.";
+}
+
     
 app.get("/", (request, response) => {
-  response.render("index", { messages });
+  response.render("index", { messages, error: "" });
 });
 
 
 app.post("/ask", (request, response) => {
   const question = request.body.question;
+  let error = "";
 
-  messages.push(question);
+  if (!question) {
+    error = "Du skal stille et spørgsmål!";
+  } else {
+    messages.push({ type: "question", text: question });
+    const answer = findAnswer(question);
+    messages.push({ type: "answer", text: answer });    
+  }
 
-  response.render("index", { messages });
+  response.render("index", { messages, error  });
 });
 
 app.listen(PORT, () => {
